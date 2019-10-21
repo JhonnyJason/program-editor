58cc00b6a169b57f56be2904286597f0af2ea769
Lenard Frommelt <lenny09918050@googlemail.com>
2019-10-21 17:26:33 +0200

    some minor updates for having code running and trying to connect to own DB

---
+++ webpack-watch.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "mode": "production",
+    "devtool": "none",
+    "target": "node",
+    "node": {
+        "__filename": true,
+        "__dirname": true
+    },
+    "optimization": {
+        "minimize": false
+    },
+    "entry": "/home/lenny/gits/program-editor/toolset/compiled/js/index.js",
+    "output": {
+        "filename": "service.js",
+        "path": "/home/lenny/gits/program-editor/output"
+    },
+    "watch": true
+}
